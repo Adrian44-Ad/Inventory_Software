86f0e69469c686bacfa5bfa9178364bfec3b48dc
Adrian44-Ad <camilobpedroza@gmail.com>
2026-03-25 17:13:46 -0500

    Uploading files

---
+++ files.py
@@ -0,0 +1,8 @@
+# modulo para administrar archivos .CSV
+import csv
+
+def save_CSV (inventory, path, include_header = True):
+    pass
+
+def load_CSV (path):
+    pass
---
+++ services.py
@@ -0,0 +1,19 @@
+# modulo que almacena las funciones de servicios
+
+def add_product(inventory, name, price, quantity):
+    pass
+
+def show_inventory(inventory):
+    pass
+
+def search_product(inventory, name):
+    pass
+
+def update_product(inventory, name, new_price = None):
+    pass
+
+def delete_product(inventory, name):
+    pass
+
+def calculate_statistics(inventory):
+    pass
